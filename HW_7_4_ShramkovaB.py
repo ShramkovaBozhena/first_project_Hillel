4d77fd1e5eddc61e82a173ce3099d28f14155bdf
ShramkovaBozhena <shramkovabozhena@gmail.com>
2026-01-20 12:15:29 +0200

    Add files via upload

---
+++ HW_7_4_ShramkovaB.py
@@ -0,0 +1,10 @@
+def common_elements():
+    my_set_one = set(range(0, 100, 3))
+    my_set_two = set(range(0, 100, 5))
+    result = my_set_one.intersection(my_set_two)
+    return result
+
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}, "Test4"
+print("OK")
+
+
