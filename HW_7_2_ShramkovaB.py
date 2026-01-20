ead566864110a48a8ec015ee289477744ebf5e1a
ShramkovaBozhena <shramkovabozhena@gmail.com>
2026-01-20 12:15:02 +0200

    Add files via upload

---
+++ HW_7_2_ShramkovaB.py
@@ -0,0 +1,12 @@
+def correct_sentence(text):
+    my_str = text[0].upper() + text[1:]
+    if my_str[-1] != ".":
+        my_str += "."
+    return my_str
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print("OK")
