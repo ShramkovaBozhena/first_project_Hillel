ce8b1b4f99592338b7d59f782217f86c93f646e3
ShramkovaBozhena <shramkovabozhena@gmail.com>
2026-01-27 19:57:47 +0200

    Add files via upload

---
+++ HW_9_2_ShramkovaB.py
@@ -0,0 +1,9 @@
+def difference(*args):
+    result = round(max(args, default = 0) - min(args, default = 0), 2)
+    return result
+
+assert difference(1, 2, 3) == 2, 'Test1' 
+assert difference(5, -5) == 10, 'Test2' 
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3' 
+assert difference() == 0, 'Test4' 
+print('OK')
