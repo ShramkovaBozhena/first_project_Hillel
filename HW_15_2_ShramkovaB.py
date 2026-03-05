f30b2745f0845358b13dd28ecb6d34bad71a0c27
ShramkovaBozhena <shramkovabozhena@gmail.com>
2026-03-05 20:48:33 +0200

    Add files via upload

---
+++ HW_15_2_ShramkovaB.py
@@ -0,0 +1,54 @@
+class Fraction:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __mul__(self, other):
+        general_a = self.a * other.a
+        general_b = self.b * other.b
+        return Fraction(general_a, general_b)
+    
+    def __add__(self, other):
+        general_b = self.b * other.b
+        general_a = self.a * other.b + other.a * self.b
+        return Fraction(general_a, general_b)
+
+    def __sub__(self, other):
+        general_b = self.b * other.b
+        general_a = self.a * other.b - other.a * self.b
+        return Fraction(general_a, general_b)
+
+    def __eq__(self, other):
+        first_fraction = self.a / self.b
+        second_fraction = other.a / other.b
+        return first_fraction == second_fraction
+
+    def __gt__(self, other):
+        first_fraction = self.a / self.b
+        second_fraction = other.a / other.b
+        return first_fraction > second_fraction
+
+    def __lt__(self, other):
+        first_fraction = self.a / self.b
+        second_fraction = other.a / other.b
+        return first_fraction < second_fraction
+
+    def __str__(self):
+        return f"Fraction: {self.a}, {self.b}"
+
+f_a = Fraction(2, 3)
+f_b = Fraction(3, 6)
+f_c = f_b + f_a
+assert str(f_c) == 'Fraction: 21, 18'
+f_d = f_b * f_a
+assert str(f_d) == 'Fraction: 6, 18'
+f_e = f_a - f_b
+assert str(f_e) == 'Fraction: 3, 18'
+
+assert f_d < f_c  # True
+assert f_d > f_e  # True
+assert f_a != f_b  # True
+f_1 = Fraction(2, 4)
+f_2 = Fraction(3, 6)
+assert f_1 == f_2  # True
+print('OK')
